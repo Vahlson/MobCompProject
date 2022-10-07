c3e3c6d154efbd666f96da84f5b5caf9b7e442d4
Joppe <jonathanbostrom@hotmail.com>
2022-10-07 16:45:52 +0200

    round corners sheet

--- lib/main.dart
+++ lib/main.dart
@@ -82,6 +82,41 @@ class _MyHomePageState extends State<MyHomePage> {
           children: [
             IconButton(icon: Icon(Icons.palette),
               onPressed: () {
+
+                showModalBottomSheet(
+                  shape: const RoundedRectangleBorder(
+                    borderRadius: BorderRadius.only( // <-- SEE HERE
+                    topLeft: Radius.circular(20.0),
+                    topRight: Radius.circular(20.0),
+                    ),
+                  ),
+                  context: context,
+                  builder: (context) {
+                    // Using Wrap makes the bottom sheet height the height of the content.
+                    // Otherwise, the height will be half the height of the screen.
+                    return Wrap(
+                      children: [
+                        ListTile(
+                          leading: Icon(Icons.share),
+                          title: Text('Share'),
+                        ),
+                        ListTile(
+                          leading: Icon(Icons.link),
+                          title: Text('Get link'),
+                        ),
+                        ListTile(
+                          leading: Icon(Icons.edit),
+                          title: Text('Edit name'),
+                        ),
+                        ListTile(
+                          leading: Icon(Icons.delete),
+                          title: Text('Delete collection'),
+                        ),
+                      ],
+                    );
+                  },
+                );
+
               }),
             IconButton(icon: Icon(Icons.architecture), onPressed: () {}),
             IconButton(icon: Icon(Icons.opacity), onPressed: () {}),
@@ -89,7 +124,7 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
       ),
 
-      floatingActionButton: FloatingActionButtoncd(
+      floatingActionButton: FloatingActionButton(
         backgroundColor: selectedColor,
         onPressed: () {
           setState(() {
